34d175a7e81f77bb4ca4cd29fbc288d78b1ee773
Demetre Iobashvili <deme.iobashvili@gmail.com>
2012-10-21 11:18:06 +0400

    5-th commit

---
+++ gui/GamePlayGrid.java
@@ -0,0 +1,62 @@
+package gui;
+
+import java.awt.Canvas;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class GamePlayGrid extends Canvas implements MouseListener{
+	
+	private static final int HEIGHT = 600; // final height of canvas
+	private static final int WIDTH = 800; // final with of canvas
+	
+	private int colCount; // number of columns of grid
+	private int rowCount; // number of rows of grid
+	
+	public GamePlayGrid(int col, int row){
+		
+	}
+	
+	// draws grid initially
+	private void drawGrid(){
+		
+	}
+	
+	// draws symbol on grid with specific coordinates
+	private void drawSymbol(char symbol, int x, int y){
+		
+	}
+
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
