2f0bd317477f03f1f88e1d1cf1819310c9d64025
Demetre Iobashvili <deme.iobashvili@gmail.com>
2012-10-21 10:58:48 +0400

    4-th commit

--- crosscutting/Main.java
+++ crosscutting/Main.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 import logic.*;
 import model.*;
 import ui.*;
-
+import gui.*;
 import ui.ConsolePresenter;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
+		MainFrame m = new MainFrame();
 		Presenter presenter = new ConsolePresenter(System.out);
 		Board board = new Board();
 		Player xPlayer = new ConsolePlayer(scanner, System.out);
--- gui/mainFrame.java
+++ gui/mainFrame.java
@@ -1,74 +1,75 @@
 package gui;
 
 import java.awt.BorderLayout;
-import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import crosscutting.*;
 
 /**
  * This is main Frame class on which initial game Panels are added
  * First game initialization panel is added, then the gameplay panel
  * */
-public class mainFrame extends JFrame{
+public class MainFrame extends JFrame{
 
-	private static final int WIDTH = 800; // width of the main window
-	private static final int HEIGHT = 600; // height of the main window
-	private static final String TITLE = "Tic Tac Toe"; // title of the application
+	private final int WIDTH = 800; // width of the main window
+	private final int HEIGHT = 600; // height of the main window
+	private final String TITLE = "Tic Tac Toe"; // title of the application
+	private JPanel mainPanel; // main panel on which all components are drawn
+	private GameInitPanel gInitPanel; // jPanel for game initialization
+	private GameplayPanel gPlayPanel;  // jPanel for GamePlay 
+	private GameStatusPanel gStatPanel; // jPanel for game stats
+	private boolean playMode; // is false if game is in init mode, ture - if the game is in Play mode
 	
-	private static mainFrame m; // main frame containing all panels
-	private static Main mainGame; // main game process
-	
-	private static JPanel mainPanel; // main panel on which all components are drawn
-	private static GameInitPanel gInitPanel; // jPanel for game initialization
-	private static GameplayPanel gPlayPanel;  // jPanel for GamePlay 
-	private static GameStatusPanel gStatPanel; // jPanel for game stats
-	
-	private static boolean playMode; // is false if game is in init mode, ture - if the game is in Play mode
-	
-	public static void main(String[] args) {
-		m = new mainFrame();
-		m.setVisible(true);
-		m.setSize(WIDTH,HEIGHT);
-		m.setTitle("BorderTest");
-		m.setLayout(new BorderLayout());
-		
+	public MainFrame(){
+		playMode = false;
+		initFrame();
 		setupMainPanel();
 		setupPanels();
 		setupInitMode();
 	}
 	
-	private static void setupMainPanel(){
-		mainPanel = new JPanel();
-		mainPanel.setLayout(new BorderLayout());
-		m.add(mainPanel, BorderLayout.CENTER);
+	private void initFrame(){
+		setVisible(true);
+		setSize(WIDTH,HEIGHT);
+		setTitle(TITLE);
+		setLayout(new BorderLayout());
 	}
 	
-	private static void setupInitMode(){
-		playMode = false;
+	private void setupMainPanel(){
+		mainPanel = new JPanel();
+		mainPanel.setLayout(new BorderLayout());
+		add(mainPanel, BorderLayout.CENTER);
+	}
+	
+	private void setupInitMode(){
+		setInitMode();
 		cleanMainPane();
 		mainPanel.add(gInitPanel, BorderLayout.CENTER);
 		mainPanel.add(gStatPanel, BorderLayout.SOUTH);
-		mainGame = new Main();
 	}
 	
-	private static void setupGameplayPanel(){
-		playMode = true;
+	private void setupGameplayPanel(){
+		setPlayMode();
 		cleanMainPane();
 		mainPanel.add(gPlayPanel, BorderLayout.CENTER);
 		mainPanel.add(gStatPanel, BorderLayout.SOUTH);
-		String[] args = null; //TODO: set args values;
-		mainGame.main(args);
 	}
 	
-	private static void setupPanels(){
+	private void setupPanels(){
 		gInitPanel = new GameInitPanel();
 		gPlayPanel = new GameplayPanel();
 		gStatPanel = new GameStatusPanel();
 	}
 	
-	private static void cleanMainPane(){
+	private void cleanMainPane(){
 		mainPanel.removeAll();
+	}
+	
+	private void setPlayMode(){
+		playMode = true;
+	}
+	
+	private void setInitMode(){
+		playMode = false;
 	}
 
 }
